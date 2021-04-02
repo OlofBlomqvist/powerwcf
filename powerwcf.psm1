09cfb9714610b2b01770c8b6df8672e5598c9977
Olof Blomqvist <olof@twnet.se>
2021-04-02 14:28:14 +0200

    Update powerwcf.psm1

--- powerwcf.psm1
+++ powerwcf.psm1
@@ -26,13 +26,18 @@ Function New-PowerWcfProxy {
     [cmdletbinding()]
     param([switch]$force,[parameter(mandatory)][string]$uri)
     Assert-WCFDeps
+    
+    
+    # Store previously generated refs in temp dir since they are slow to generate. use the force to override.
+    # hashing it to generate a good filename since some characters in url might now be allowed by the filesystem.
     $tempPath = [System.IO.Path]::GetTempPath()
     $md5 = New-Object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider
     $utf8 = New-Object -TypeName System.Text.UTF8Encoding
     $hash = [System.BitConverter]::ToString($md5.ComputeHash($utf8.GetBytes($uri))) -replace "-",""
     $filename = "$($hash).cs"
     $tempFilePath = Join-Path -Path $tempPath -ChildPath $filename
-
+    
+    # allow re-generating refs incase service has changed
     if($force -and (test-path $tempFilePath)) {
         Remove-Item $tempFilePath -Force -Confirm:$false -ErrorAction Stop
     }
@@ -40,6 +45,9 @@ Function New-PowerWcfProxy {
     if(test-path $tempFilePath) {
         write-host -ForegroundColor green "Reference source for this uri already exists, re-using it. You can use the -force flag to re-generate it."
     } else {
+        # we only want to expose actual service client types in the proxy object.
+        # we use a semi-random typename because we can't unload already imported types in powershell
+        # and so if user wants to re-generate refs we need a new namespace to avoid collisions.
         $typename = "*,WCFPROXYTYPE.X$([System.Guid]::NewGuid().ToString())"  -replace "-",""
         Write-Verbose "Invoking command: 'dotnet-svcutil $uri --noStdLib -d $tempPath -o $filename` -n `"$typename`"'"
         
@@ -47,13 +55,16 @@ Function New-PowerWcfProxy {
         if($? -eq $false) {write-error $x;throw "failed to generate reference"}
     }
 
-    $data = (Get-Content -raw $tempFilePath)
+    $data = Get-Content -raw $tempFilePath
 
+    # In newer versions it seems we must reference system.private.servicemodel
+    # or .net will puke some garbage..
     $svcmodnamespace = "System.ServiceModel"
     if($PSVersionTable.PSVersion.Major -gt 5) {
         $svcmodnamespace = "System.Private.ServiceModel"
     }
 
+    # compile and import the generated source refs to powershell
     $t = add-type -TypeDefinition $data -ReferencedAssemblies @(
         "System.Runtime.Serialization",
         "System.ServiceModel.Primitives",
@@ -65,11 +76,14 @@ Function New-PowerWcfProxy {
         "System.Collections"
     ) -PassThru -ErrorAction Stop
     
+    # The binding here should likely be possible to configure using input arguments
+    # but thats out of scope for this poc.
     $proxyObject = New-Object -TypeName "PSCustomObject"
-
     $binding = [System.ServiceModel.BasicHttpBinding]::new()
     $binding.MaxReceivedMessageSize = 999999
     
+    # populate proxy object with all generated service client objects available
+    # and make all methods of the client instancces visible as default members before returning it
     $t | Where-Object basetype -imatch "clientbase*\[WCFPROXY" | ForEach-Object {
         $serviceClientInstance = [scriptblock]::Create("[$($_.fullname)]::new(`$binding,`$uri)").Invoke()|Select-Object -first 1
         $defaultDisplaySet = $serviceClientInstance|Get-Member -MemberType method | Select-Object -ExpandProperty Name
